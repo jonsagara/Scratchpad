254907ea45a2fb35678d9a40e7deaa062daf9e5a
Jon Sagara <jon@sagarasoftware.com>
2022-04-03 09:22:07 -0700

    Added some more tests.

--- CSharp/StringHelpers/src/StringHelpers.Tests/RandomStringHelperTests.cs
+++ CSharp/StringHelpers/src/StringHelpers.Tests/RandomStringHelperTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Buffers.Text;
+using Xunit;
 
 namespace StringHelpers.Tests;
 
@@ -55,6 +56,155 @@ public class RandomStringHelperTests
         for (var digitChar = '0'; digitChar <= '9'; digitChar++)
         {
             Assert.Contains(digitChar, digitsLookup);
+        }
+    }
+
+
+    //
+    // Ensure that we can't pass an invalid length or byte count.
+    //
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void GenerateRandomBase64UrlEncodedString_InvalidByteCountThrows(int invalidByteCount)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => RandomStringHelper.GenerateRandomBase64UrlEncodedString(invalidByteCount));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void GenerateRandomBase64EncodedString_InvalidByteCountThrows(int invalidByteCount)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => RandomStringHelper.GenerateRandomBase64EncodedString(invalidByteCount));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void GenerateUppercaseAlphanumericString_InvalidLengthThrows(int invalidLength)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => RandomStringHelper.GenerateUppercaseAlphanumericString(invalidLength));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void GenerateAlphanumericString_InvalidLengthThrows(int invalidLength)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => RandomStringHelper.GenerateAlphanumericString(invalidLength));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void GenerateRandomString_InvalidLengthThrows(int invalidLength)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => RandomStringHelper.GenerateRandomString(invalidLength));
+    }
+
+
+    //
+    // Ensure the length of the returned string matches the expected length.
+    //
+
+    //[Theory]
+    //[InlineData(1)]
+    //[InlineData(10)]
+    //[InlineData(32)]
+    //[InlineData(63)]
+    //[InlineData(64)]
+    //[InlineData(128)]
+    //[InlineData(256)]
+    //public void GenerateRandomBase64UrlEncodedString_ReturnedStringLengthMatches(int length)
+    //{
+    //    var generatedString = RandomStringHelper.GenerateRandomBase64UrlEncodedString(length);
+
+    //    Assert.NotNull(generatedString);
+    //    Assert.Equal(length, generatedString.Length);
+    //}
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(32)]
+    [InlineData(63)]
+    [InlineData(64)]
+    [InlineData(128)]
+    [InlineData(256)]
+    public void GenerateRandomBase64EncodedString_ReturnedStringLengthMatches(int length)
+    {
+        var expectedLength = Base64.GetMaxEncodedToUtf8Length(length);
+        var generatedString = RandomStringHelper.GenerateRandomBase64EncodedString(length);
+
+        Assert.NotNull(generatedString);
+        Assert.Equal(expectedLength, generatedString.Length);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(32)]
+    [InlineData(63)]
+    [InlineData(64)]
+    [InlineData(128)]
+    [InlineData(256)]
+    public void GenerateUppercaseAlphanumericString_ReturnedStringLengthMatches(int length)
+    {
+        var generatedString = RandomStringHelper.GenerateUppercaseAlphanumericString(length);
+
+        Assert.NotNull(generatedString);
+        Assert.Equal(length, generatedString.Length);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(32)]
+    [InlineData(63)]
+    [InlineData(64)]
+    [InlineData(128)]
+    [InlineData(256)]
+    public void GenerateAlphanumericString_ReturnedStringLengthMatches(int length)
+    {
+        var generatedString = RandomStringHelper.GenerateAlphanumericString(length);
+
+        Assert.NotNull(generatedString);
+        Assert.Equal(length, generatedString.Length);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(32)]
+    [InlineData(63)]
+    [InlineData(64)]
+    [InlineData(128)]
+    [InlineData(256)]
+    public void GenerateRandomString_ReturnedStringLengthMatches(int length)
+    {
+        var randomString = RandomStringHelper.GenerateRandomString(length);
+
+        Assert.NotNull(randomString);
+        Assert.Equal(length, randomString.Length);
+    }
+
+
+    //
+    // Ensure the returned string has no dashes and no underscores.
+    //
+
+    [Fact]
+    public void GenerateAlphanumericString_ReturnsStringWithNoDashesAndNoUnderscores()
+    {
+        for (var ixIteration = 0; ixIteration < 100; ixIteration++)
+        {
+            var randomString = RandomStringHelper.GenerateAlphanumericString(length: 64, includeDashAndUnderscore: false);
+
+            Assert.NotNull(randomString);
+            Assert.DoesNotContain("-", randomString);
+            Assert.DoesNotContain("_", randomString);
         }
     }
 }
