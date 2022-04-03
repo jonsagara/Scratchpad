e676fdce8d30005e6b30186e98e6c60d3a3b2dc2
Jon Sagara <jon@sagarasoftware.com>
2022-04-02 18:41:57 -0700

    Use Guid.TryWriteBytes instead of MemoryMarshall directly.

--- CSharp/StringHelpers/src/StringHelpers/StringEncodingHelper.cs
+++ CSharp/StringHelpers/src/StringHelpers/StringEncodingHelper.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.WebUtilities;
 
 namespace StringHelpers;
 
@@ -10,7 +9,11 @@ public static class StringEncodingHelper
         // Copying the Guid bytes into a stack-allocated array allocates far fewer bytes
         //   than calling ToByteArray().
         Span<byte> idBytes = stackalloc byte[16];
-        MemoryMarshal.TryWrite(idBytes, ref id);
+
+        if (!id.TryWriteBytes(idBytes))
+        {
+            throw new InvalidOperationException($"Unable to copy Guid '{id}' bytes to a stackalloc byte[16].");
+        }
 
         return WebEncoders.Base64UrlEncode(idBytes);
     }
