d68d0ad2554b503b87d3eb0de20b030e37159f3e
Jon Sagara <jon@sagarasoftware.com>
2022-04-04 17:21:43 -0700

    The optimized version of SequentialGuid now uses Spans, stackalloc, and BinaryPrimitives to eliminate heap allocations.

--- CSharp/SequentialGuids/src/SequentialGuids/SequentialGuid_Optimized.cs
+++ CSharp/SequentialGuids/src/SequentialGuids/SequentialGuid_Optimized.cs
@@ -4,36 +4,49 @@ namespace SequentialGuids;
 
 public class SequentialGuid_Optimized
 {
-    private static readonly long _epochTicks = new DateTime(1900, 1, 1).Ticks;
+    /// <summary>
+    /// Equivalent to 'SELECT CAST(0 AS DATETIME)'. Specify UTC since we're using UTC in GenerateComb, and 
+    /// since all the servers we run on are set to UTC.
+    /// </summary>
+    private static readonly long _epochTicks = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+
     private static readonly int[] _sqlOrderMap = new int[16] { 3, 2, 1, 0, 5, 4, 7, 6, 9, 8, 15, 14, 13, 12, 11, 10 };
 
     public static Guid GenerateComb()
     {
-        DateTime now = DateTime.UtcNow;
-        TimeSpan timeSinceEpoch = new TimeSpan(now.Ticks - _epochTicks);
-        TimeSpan timeOfDay = now.TimeOfDay;
+        //
+        // NOTE: the last 8 bytes of a UNIQUEIDENTIFIER in SQL Server are stored as Big Endian. Since we're
+        //   overwriting the last 6 bytes, we need to convert those numbers to Big Endian.
+        //
+        //  Bits    Bytes   Name    Endianness
+        //  32      4       Data1   Native
+        //  16      2       Data2   Native
+        //  16      2       Data3   Native
+        //  64      8       Data4   Big
+        //
+        // Native on Windows means Little Endian.
+        //
+        // See: https://dba.stackexchange.com/a/121878
+        //
 
-        // Generate a new Guid. We'll alter several of its bytes to suit our purposes.
+        // Generate a new Guid. We'll alter its last 6 bytes to make it less random and more sequential.
         Span<byte> guidBytes = stackalloc byte[16];
         Guid.NewGuid().TryWriteBytes(guidBytes);
 
-        // Get the days and milliseconds which will be used to build the byte string.
-        // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333 
-        //Span<byte> daysBytes = BitConverter.GetBytes(timeSinceEpoch.Days);
-        //Span<byte> msecsBytes = BitConverter.GetBytes((long)(timeOfDay.TotalMilliseconds / 3.333333));
+        var now = DateTime.UtcNow;
 
-        //// Reverse the bytes to match SQL Servers ordering 
-        //daysBytes.Reverse();
-        //msecsBytes.Reverse();
+        // Get the days elapsed since the SQL Server DATETIME epoch.
+        var timeSinceEpoch = new TimeSpan(now.Ticks - _epochTicks);
         Span<byte> daysBytes = stackalloc byte[4];
         BinaryPrimitives.WriteInt32BigEndian(daysBytes, timeSinceEpoch.Days);
 
+        // Get the current time of day.
+        // NOTE: SQL Server is accurate to 1/300th of a millisecond, so we divide by 3.333333 
+        TimeSpan timeOfDay = now.TimeOfDay;
         Span<byte> msecsBytes = stackalloc byte[8];
         BinaryPrimitives.WriteInt64BigEndian(msecsBytes, (long)(timeOfDay.TotalMilliseconds / 3.333333));
 
-        // Copy the bytes into the guid 
-        //Array.Copy(sourceArray: daysBytes, sourceIndex: daysBytes.Length - 2, destinationArray: guidBytes, destinationIndex: guidBytes.Length - 6, length: 2);
-        //Array.Copy(sourceArray: msecsBytes, sourceIndex: msecsBytes.Length - 4, destinationArray: guidBytes, destinationIndex: guidBytes.Length - 4, length: 4);
+        // Copy the days and milliseconds bytes into the end of the Guid, starting at offset 10.
         daysBytes.Slice(start: daysBytes.Length - 2, length: 2).CopyTo(guidBytes.Slice(start: guidBytes.Length - 6));
         msecsBytes.Slice(start: msecsBytes.Length - 4, length: 4).CopyTo(guidBytes.Slice(start: guidBytes.Length - 4));
 
