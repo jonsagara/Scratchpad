12f75199e2e41a79ad5cb0276a2f6ed88ca4ac15
Jon Sagara <jon@sagarasoftware.com>
2022-09-19 18:56:22 -0700

    Made toSlug functional.

--- FSharp/StringHelpers/src/StringHelpers/StringHelper.fs
+++ FSharp/StringHelpers/src/StringHelpers/StringHelper.fs
@@ -34,6 +34,60 @@ module StringHelper =
                 .Normalize(NormalizationForm.FormC)
 
     /// <summary>
+    /// Returns true if the character is NOT a non-spacing mark; false if it is.
+    /// </summary>
+    /// <remarks>
+    /// Example of a non-spacing mark: codepoint 786 (&quot;Combining grave accent&quot;).
+    /// </remarks>
+    let isNotNonSpacingMark (c : char) =
+        CharUnicodeInfo.GetUnicodeCategory(c) <> UnicodeCategory.NonSpacingMark
+
+    /// <summary>
+    /// Normalize the string to FormD, in which an accented character is separated into different codepoints.
+    /// </summary>
+    /// <remarks>
+    /// For special UNICODE characters, separate them into different code points. Ex: &quot;à&quot; becomes codepoint 97 
+    /// (&quot;Latin small letter A&quot;) followed by codepoint 786 (&quot;Combining grave accent&quot;).
+    /// See: https://stackoverflow.com/a/3288164
+    /// </remarks>
+    let normalizeToFormD (s : string) = s.Normalize(NormalizationForm.FormD)
+
+    /// <summary>
+    /// Normalize the string to FormC, in which an accented character is represented by a single codepoint.
+    /// </summary>
+    /// <remarks>
+    /// For special UNICODE characters, form C uses a single letter-with-accent codepoint. For example,
+    /// &quot;à&quot; can be codepoint 224 (&quot;Latin small letter A with grave&quot;).
+    /// </remarks>
+    let normalizeToFormC (s : string) = s.Normalize(NormalizationForm.FormC)    
+
+    /// <summary>
+    /// <para>Remove diacritics from the given text.</para>
+    /// <para>See: https://stackoverflow.com/a/249126 </para>
+    /// </summary>
+    let removeDiacriticsFunctional (text: string) =
+        if isNull (text) then
+            null
+        else
+            text
+            // Normalize accented characters into separate codepoints (base character and non-spacing mark).
+            |> normalizeToFormD
+            // Keep any characters that are NOT a non-spacing mark (e.g., the Combining grave accent mentioned above).
+            //   IOW, filter out any non-spacing marks.
+            |> Seq.filter isNotNonSpacingMark
+            // Convert back to a character array and then a proper String.
+            |> Seq.toArray
+            |> String
+            // Normalize accented characters into a single codepoint.
+            |> normalizeToFormC
+
+    let private maxSlugLength maxLengthInChars (slug: string) =
+        if slug.Length <= maxLengthInChars then
+            slug.Length
+        else
+            maxLengthInChars
+
+    /// <summary>
     /// <para>Convert a string into a URL- and file name-safe slug.</para>
     /// <para>See: https://stackoverflow.com/a/2921135 (except ours doesn't use RemoveAccent)</para>
     /// </summary>
@@ -53,19 +107,75 @@ module StringHelper =
             // Limit the slug length to the max specified characters.
             slug <-
                 slug
-                    .Substring(
-                        0,
-                        if slug.Length <= maxLengthInChars then
-                            slug.Length
-                        else
-                            maxLengthInChars
-                    )
+                    .Substring(0, slug |> maxSlugLength maxLengthInChars)
                     .Trim()
 
             // Replace white space with hyphens.
             slug <- Regex.Replace(slug, @"\s", "-")
 
             slug
+
+    /// <summary>
+    /// Remove characters that are non-alphanumeric, white space, or hyphens.
+    /// </summary>
+    let private removeInvalidCharacters (value : string) =
+        Regex.Replace(value, @"[^a-zA-Z0-9\s-]", String.Empty)
+
+    /// <summary>
+    /// Collapse multiple consecutive white space values into a single space.
+    /// </summary>
+    let private collapseMultipleWhiteSpaceToSingleSpace (value : string) =
+        Regex.Replace(value, @"\s+", " ")
+
+    /// <summary>
+    /// If null, return null. Otherwise, trim any leading and trailing white space.
+    /// </summary>
+    let private nullSafeTrim (value : string) =
+        match isNull(value) with
+        | true -> null
+        | false -> value.Trim()
+
+    /// <summary>
+    /// Limit the length of the slug to a maximum number of characters.
+    /// </summary>
+    let private truncateSlug (maxLengthInChars : int) (value : string) =
+        let slugLength =
+            if value.Length <= maxLengthInChars then
+                value.Length
+            else
+                maxLengthInChars
+
+        value.Substring(0, slugLength)
+
+    /// <summary>
+    /// Remove characters that are non-alphanumeric, white space, or hyphens.
+    /// </summary>
+    let private replaceWhiteSpaceWithHyphens (value : string) =
+        Regex.Replace(value, @"\s", "-")
+
+    /// <summary>
+    /// <para>Convert a string into a URL- and file name-safe slug.</para>
+    /// <para>See: https://stackoverflow.com/a/2921135 (except ours doesn't use RemoveAccent)</para>
+    /// </summary>
+    let private toSlugInternalFunctional (maxLengthInChars: int) (value: string) =
+        if isNull (value) then
+            null
+        else
+            value
+            // We know we're passing in a non-null string, so we're going to get a non-null string back.
+            |> removeDiacriticsFunctional
+            // Remove invalid chars.
+            |> removeInvalidCharacters
+            // Convert multiple spaces into one space.
+            |> collapseMultipleWhiteSpaceToSingleSpace
+            // Trim any leading and trailing white space.
+            |> nullSafeTrim
+            // Truncate the slug at the maximum allowed characters.
+            |> truncateSlug maxLengthInChars
+            // Trim any leading and trailing white space from the truncated slug.
+            |> nullSafeTrim
+            // Replace white space with hyphens.
+            |> replaceWhiteSpaceWithHyphens
 
     /// <summary>
     /// Convert a string into a URL- and file name-safe slug. Constrain the resulting string to the specified maximum length.
--- FSharp/StringHelpers/src/StringHelpers/StringHelper.fsx
+++ FSharp/StringHelpers/src/StringHelpers/StringHelper.fsx
@@ -11,8 +11,8 @@ let private MAX_SLUG_CHAR_LENGTH = 50
 /// <para>Remove diacritics from the given text.</para>
 /// <para>See: https://stackoverflow.com/a/249126 </para>
 /// </summary>
-let removeDiacritics (text : string) =
-    if isNull(text) then
+let removeDiacritics (text: string) =
+    if isNull (text) then
         null
     else
         let normalizedString = text.Normalize(NormalizationForm.FormD)
@@ -20,29 +20,63 @@ let removeDiacritics (text : string) =
 
         // Only keep strings that are non-spacing marks.
         normalizedString
-        |> Seq.iter (fun c -> 
+        |> Seq.iter (fun c ->
             let unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c)
-            if unicodeCategory <> UnicodeCategory.NonSpacingMark then do
-                stringBuilder.Append c |> ignore
-            )
 
-        stringBuilder.ToString().Normalize(NormalizationForm.FormC)
+            if unicodeCategory <> UnicodeCategory.NonSpacingMark then
+                do stringBuilder.Append c |> ignore)
 
-let removeDiacriticsFunctional (text : string) =
-    if isNull(text) then
+        stringBuilder
+            .ToString()
+            .Normalize(NormalizationForm.FormC)
+
+/// <summary>
+/// Returns true if the character is NOT a non-spacing mark; false if it is.
+/// </summary>
+/// <remarks>
+/// Example of a non-spacing mark: codepoint 786 (&quot;Combining grave accent&quot;).
+/// </remarks>
+let isNotNonSpacingMark (c : char) =
+    CharUnicodeInfo.GetUnicodeCategory(c) <> UnicodeCategory.NonSpacingMark
+
+/// <summary>
+/// Normalize the string to FormD, in which an accented character is separated into different codepoints.
+/// </summary>
+/// <remarks>
+/// For special UNICODE characters, separate them into different code points. Ex: &quot;à&quot; becomes codepoint 97 
+/// (&quot;Latin small letter A&quot;) followed by codepoint 786 (&quot;Combining grave accent&quot;).
+/// See: https://stackoverflow.com/a/3288164
+/// </remarks>
+let normalizeToFormD (s : string) = s.Normalize(NormalizationForm.FormD)
+
+/// <summary>
+/// Normalize the string to FormC, in which an accented character is represented by a single codepoint.
+/// </summary>
+/// <remarks>
+/// For special UNICODE characters, form C uses a single letter-with-accent codepoint. For example,
+/// &quot;à&quot; can be codepoint 224 (&quot;Latin small letter A with grave&quot;).
+/// </remarks>
+let normalizeToFormC (s : string) = s.Normalize(NormalizationForm.FormC)    
+
+/// <summary>
+/// <para>Remove diacritics from the given text.</para>
+/// <para>See: https://stackoverflow.com/a/249126 </para>
+/// </summary>
+let removeDiacriticsFunctional (text: string) =
+    if isNull (text) then
         null
     else
-        let nonDiacriticsChars =
-            text.Normalize(NormalizationForm.FormD)
-            |> Seq.filter (fun c ->
-                // Filter out diacritics (non-spacing characters that indiciate modifiations of a base character).
-                let unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c)
-                unicodeCategory <> UnicodeCategory.NonSpacingMark
-                )
-            |> Seq.toArray
-            |> String
-
-        nonDiacriticsChars.Normalize(NormalizationForm.FormC)
+        text
+        // Normalize accented characters into separate codepoints (base character and non-spacing mark).
+        |> normalizeToFormD
+        // Keep any characters that are NOT a non-spacing mark (e.g., the Combining grave accent mentioned above).
+        //   IOW, filter out any non-spacing marks.
+        |> Seq.filter isNotNonSpacingMark
+        // Convert back to a character array and then a proper String.
+        |> Seq.toArray
+        |> String
+        // Normalize accented characters into a single codepoint.
+        |> normalizeToFormC
 
 
 let dessert = "crème brûlée"
