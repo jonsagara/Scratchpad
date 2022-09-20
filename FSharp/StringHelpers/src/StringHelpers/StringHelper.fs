a13dd426315f585a1391975e6d0dac13909363a6
Jon Sagara <jon@sagarasoftware.com>
2022-09-19 18:59:33 -0700

    Removed non-functional versions.

--- FSharp/StringHelpers/src/StringHelpers/StringHelper.fs
+++ FSharp/StringHelpers/src/StringHelpers/StringHelper.fs
@@ -11,29 +11,6 @@ module StringHelper =
     let private MAX_SLUG_CHAR_LENGTH = 50
 
     /// <summary>
-    /// <para>Remove diacritics from the given text.</para>
-    /// <para>See: https://stackoverflow.com/a/249126 </para>
-    /// </summary>
-    let private removeDiacritics (text: string) =
-        if isNull (text) then
-            null
-        else
-            let normalizedString = text.Normalize(NormalizationForm.FormD)
-            let stringBuilder = StringBuilder()
-
-            // Only keep strings that are non-spacing marks.
-            normalizedString
-            |> Seq.iter (fun c ->
-                let unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c)
-
-                if unicodeCategory <> UnicodeCategory.NonSpacingMark then
-                    do stringBuilder.Append c |> ignore)
-
-            stringBuilder
-                .ToString()
-                .Normalize(NormalizationForm.FormC)
-
-    /// <summary>
     /// Returns true if the character is NOT a non-spacing mark; false if it is.
     /// </summary>
     /// <remarks>
@@ -65,7 +42,7 @@ module StringHelper =
     /// <para>Remove diacritics from the given text.</para>
     /// <para>See: https://stackoverflow.com/a/249126 </para>
     /// </summary>
-    let removeDiacriticsFunctional (text: string) =
+    let removeDiacritics (text: string) =
         if isNull (text) then
             null
         else
@@ -80,40 +57,6 @@ module StringHelper =
             |> String
             // Normalize accented characters into a single codepoint.
             |> normalizeToFormC
-
-    let private maxSlugLength maxLengthInChars (slug: string) =
-        if slug.Length <= maxLengthInChars then
-            slug.Length
-        else
-            maxLengthInChars
-
-    /// <summary>
-    /// <para>Convert a string into a URL- and file name-safe slug.</para>
-    /// <para>See: https://stackoverflow.com/a/2921135 (except ours doesn't use RemoveAccent)</para>
-    /// </summary>
-    let private toSlugInternal (maxLengthInChars: int) (value: string) =
-        if isNull (value) then
-            null
-        else
-            // We know we're passing in a non-null string, so we're going to get a non-null string back.
-            let mutable slug = removeDiacritics value
-
-            // Remove invalid chars.
-            slug <- Regex.Replace(slug, @"[^a-zA-Z0-9\s-]", String.Empty)
-
-            // Convert multiple spaces into one space.
-            slug <- Regex.Replace(slug, @"\s+", " ").Trim()
-
-            // Limit the slug length to the max specified characters.
-            slug <-
-                slug
-                    .Substring(0, slug |> maxSlugLength maxLengthInChars)
-                    .Trim()
-
-            // Replace white space with hyphens.
-            slug <- Regex.Replace(slug, @"\s", "-")
-
-            slug
 
     /// <summary>
     /// Remove characters that are non-alphanumeric, white space, or hyphens.
@@ -157,16 +100,16 @@ module StringHelper =
     /// <para>Convert a string into a URL- and file name-safe slug.</para>
     /// <para>See: https://stackoverflow.com/a/2921135 (except ours doesn't use RemoveAccent)</para>
     /// </summary>
-    let private toSlugInternalFunctional (maxLengthInChars: int) (value: string) =
+    let private toSlugInternal (maxLengthInChars: int) (value: string) =
         if isNull (value) then
             null
         else
             value
             // We know we're passing in a non-null string, so we're going to get a non-null string back.
-            |> removeDiacriticsFunctional
+            |> removeDiacritics
             // Remove invalid chars.
             |> removeInvalidCharacters
-            // Convert multiple spaces into one space.
+            // Collapse multiple consecutive white spaces into one space.
             |> collapseMultipleWhiteSpaceToSingleSpace
             // Trim any leading and trailing white space.
             |> nullSafeTrim
