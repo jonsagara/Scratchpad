ec15a7d5b87e0b85936a835923546b967535cebd
Jon Sagara <jon@sagarasoftware.com>
2022-11-17 09:21:24 -0800

    Added the new file.

---
+++ FSharp/StringHelpers/src/StringHelpers/ByteExtensions.fs
@@ -0,0 +1,21 @@
+﻿namespace StringHelpers
+
+open System
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<Extension>]
+type ByteExtensions =
+
+    /// <summary>
+    /// Convert each byte to its equivalent hexadecimal string representation, and return them all as a string.
+    /// </summary>
+    /// <remarks>
+    /// The extra parameter attributes are necessary so that C# sees includeDashes as an optional bool, not an FSharpOption.
+    /// See: https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/parameters-and-arguments#optional-parameters
+    /// </remarks>
+    [<Extension>]
+    static member ToHexString (bytes: byte[], [<Optional; DefaultParameterValue(false)>] includeDashes: bool) =
+        match includeDashes with
+        | true -> BitConverter.ToString(bytes)
+        | false -> BitConverter.ToString(bytes).Replace("-", String.Empty)
